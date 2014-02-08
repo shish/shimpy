88f72542b114d98df3c0bbc38b18c30e41fed8b0
Shish <shish@shishnet.org>
2014-02-08 21:17:20 +0000

    some bbcode formatting

--- shimpy/ext/bbcode/__init__.py
+++ shimpy/ext/bbcode/__init__.py
@@ -1,10 +1,17 @@
 from shimpy.core.extension import FormatterExtension
+from shimpy.core.utils import make_link
+import bbcode
 
 
 class BBCode(FormatterExtension):
+    def __init__(self):
+        FormatterExtension.__init__(self)
+        self.parser = bbcode.Parser()
+
     # TODO
     def format(self, text):
-        return text
+        text = text.replace("site://", make_link("/"))
+        return self.parser.format(text)
 
     def strip(self, text):
         return text
